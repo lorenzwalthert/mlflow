7cbb1ba13b01f55dc1dd9852911cf4ccc0ff2a5e
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 19:01:05 +0100

    only use mlflow conda env after keras is installed.

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -1,6 +1,4 @@
 library(reticulate)
-use_condaenv(mlflow:::mlflow_conda_env_name())
-
 parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
@@ -12,4 +10,5 @@ install_keras(method = "conda", tensorflow="1.14.0")
 # -------------------------------------------------- REMOVE COMMENT ------------
 #devtools::check_built(path = package, error_on = "note", args = "--no-tests")
 # source("testthat.R")
+use_condaenv(mlflow:::mlflow_conda_env_name())
 pyfunc <- import("mlflow.pyfunc")
