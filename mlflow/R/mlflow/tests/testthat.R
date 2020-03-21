6808d7daf78c7f4fe8ce673b83b00f62753f6061
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 18:15:33 +0100

    try to use right conda env

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -1,12 +1,13 @@
 library(testthat)
 library(mlflow)
 library(reticulate)
+use_condaenv(mlflow:::mlflow_conda_env_name())
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   message("Current working directory: ", getwd())
   # needed for test that uses mlflow cli
   conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
-  use_condaenv(mlflow:::mlflow_conda_env_name())
+
   pyfunc <- import("mlflow.pyfunc")
   # test_check("mlflow")
 }
