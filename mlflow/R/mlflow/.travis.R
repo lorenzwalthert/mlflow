449c3ce08a0e5f3c3726de9703a0b0c79a41abc0
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 18:23:37 +0100

    activate mlflow before installing keras

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -3,6 +3,8 @@ package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
 install.packages("keras", repos='http://cran.rstudio.com/')
 install.packages("roxygen2")
+use_condaenv(mlflow:::mlflow_conda_env_name())
+
 library(keras)
 # pinning tensorflow version to 1.14 until test_keras_model.R is fixed
 install_keras(method = "conda", tensorflow="1.14.0")
