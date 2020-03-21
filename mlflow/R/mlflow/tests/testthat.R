8f770364548f4177e3879b8ff702f7458c531feb
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 19:17:09 +0100

    go back to full testing

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -6,7 +6,5 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   message("Current working directory: ", getwd())
   # needed for test that uses mlflow cli
   conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
-
-
   test_check("mlflow")
 }
