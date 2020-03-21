e5282594d4af7b662f0acad976d2dd3535b77ec6
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 15:13:43 +0100

    install mlflow in R session before running tests because linking seems not to work when python already loaded

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -5,13 +5,8 @@ library(reticulate)
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   mlflow:::mlflow_maybe_create_conda_env(python_version = "3.6")
   message("Current working directory: ", getwd())
-  mlflow_home <- Sys.getenv("MLFLOW_HOME", "../../../../.")
-  conda_install(c(mlflow_home), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
   # needed for test that uses mlflow cli
   conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
-  conda_create("r-test")
-  use_condaenv("r-test")
   use_condaenv(mlflow:::mlflow_conda_env_name())
-  conda_remove("r-test")
   test_check("mlflow")
 }
