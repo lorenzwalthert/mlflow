f28e6161a44f8d1405b9b19ccd8b99ec2c98c8ee
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 19:10:14 +0100

    move installation to separate R session because keras will use other env than mlflow

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -1,14 +1,10 @@
 library(reticulate)
+use_condaenv(mlflow:::mlflow_conda_env_name())
 parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
-install.packages("keras", repos='http://cran.rstudio.com/')
 install.packages("roxygen2")
-library(keras)
-# # pinning tensorflow version to 1.14 until test_keras_model.R is fixed
-install_keras(method = "conda", tensorflow="1.14.0")
 # -------------------------------------------------- REMOVE COMMENT ------------
 #devtools::check_built(path = package, error_on = "note", args = "--no-tests")
 # source("testthat.R")
-use_condaenv(mlflow:::mlflow_conda_env_name())
 pyfunc <- import("mlflow.pyfunc")
