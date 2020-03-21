872b536fefb7e2b3cffb6a5b31df5a9fcce6dff2
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 15:52:48 +0100

    build python package in previous R session

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -1,3 +1,6 @@
+parent_dir <- dir("../", full.names = TRUE)
+package <- parent_dir[grepl("mlflow_", parent_dir)]
+install.packages(package)
 install.packages("keras", repos='http://cran.rstudio.com/')
 install.packages("roxygen2")
 library(keras)
--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -3,7 +3,6 @@ library(mlflow)
 library(reticulate)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  mlflow:::mlflow_maybe_create_conda_env(python_version = "3.6")
   message("Current working directory: ", getwd())
   # needed for test that uses mlflow cli
   conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
