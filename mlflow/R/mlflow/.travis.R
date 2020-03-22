44134b74d17736bac8bd4d153d214f1e68821c57
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-22 18:43:41 +0100

    don't require conda env explicitly, use test env

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -1,5 +1,4 @@
 library(reticulate)
-use_condaenv(mlflow:::mlflow_conda_env_name())
 parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
