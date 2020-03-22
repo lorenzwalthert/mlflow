ad51b091439cc3f73a72ae88048b108d618161ca
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-22 15:08:15 +0100

    try to make sure base version of python match

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -5,6 +5,5 @@ library(mlflow)
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   message("Current working directory: ", getwd())
   # needed for test that uses mlflow cli
-  conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
   test_check("mlflow")
 }
